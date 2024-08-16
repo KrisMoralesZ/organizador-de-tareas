5777c421b38924f4c5ff16f9691c0ed33ac9b445
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-08-16 13:51:24 -0700

    update task and user model with mongoid

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,60 +1,29 @@
-# == Schema Information
-#
-# Table name: tasks
-#
-#  id          :bigint           not null, primary key
-#  code        :string
-#  description :string
-#  due_date    :date
-#  name        :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  category_id :bigint           not null
-#  owner_id    :bigint           not null
-#
-# Indexes
-#
-#  index_tasks_on_category_id  (category_id)
-#  index_tasks_on_owner_id     (owner_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (category_id => categories.id)
-#  fk_rails_...  (owner_id => users.id)
-#
-class Task < ApplicationRecord
-  # //-------------------- Associations --------------------
-  has_many :notes
+class Task
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :description, type: String
+  field :due_date, type: Date
+  field :code, type: string
 
   belongs_to :category
-
-  # The 'owner' is a user, so we need to specify the class_name of the model
   belongs_to :owner, class_name: 'User'
-
-  # A task can have many participants
   has_many :participating_users, class_name: 'Participant', dependent: :destroy # Here we have to specify the model class name
-  has_many :participants, through: :participating_users, source: :user # When using the option "through"....we can't ->
-  # -> use the "class_name" option instead we use the "source" option
-  # //-------------------- Associations --------------------
+  has_many :notes
 
-  # //-------------------- Validations --------------------
-  # note: "validates" allow us to use built in rails validations
-
-  # NOTE: We can also validates associations
   validates :participating_users, presence: true
-
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-
-  # To allow the creation of nested attributes for the associated model
-  # we need to use the method "accepts_nested_attributes_for"
-  # The option "allow_destroy" allow us to delete the associated model if we delete a participating user...the task ->
-  # -> will not show up as a participant anymore.
-  accepts_nested_attributes_for :participating_users, allow_destroy: true
-
-  # *----- Custom validations
-  # note: "validate" allow us to use custom validations
   validate :due_date_not_in_past
 
+  before_create :create_code
+  after_create :send_email
+
+  accepts_nested_attributes_for :participating_users, allow_destroy: true
+
+  def participants
+    participating_users.includes(:user).map(&:user)
+  end
   def due_date_not_in_past
     return if due_date.blank?
     return if due_date > Date.today
@@ -62,27 +31,13 @@ class Task < ApplicationRecord
     # errors.add() add an error message to the object errors collection for the model
     errors.add(:due_date, I18n.t('task.errors.due_date_not_in_past'))
   end
-
-  # *----- Custom validations
-  # //-------------------- Validations --------------------
-
-  # //-------------------- Callbacks --------------------
-  # Callbacks are always define after the validations
-  before_create :create_code
-  after_create :send_email
-  # //-------------------- Callbacks --------------------
-
-  # //-------------------- Methods --------------------
   def create_code
     self.code = "#{owner_id}#{Time.now.to_i.to_s(36)}#{SecureRandom.hex(8)}"
   end
 
-  # This function/method will be trigger the mailer
   def send_email
+    return
     return unless Rails.env.development?
-
-    # As we know the 'participants' is an array so we just add the 'owner' to it. And then loop the array and send a ->
-    #-> email to each of them.
     (participants + [owner]).each do |user|
       ParticipantMailer.with(user: user, task: self).new_task_email.deliver! # trigger the mailer
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,43 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
-# Indexes
-#
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#
-class User < ApplicationRecord
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # *-------------------- Associations --------------------
-  # A owner(user) has many tasks
-  has_many :owned_tasks, class_name: 'Task', foreign_key: 'owner_id' # Here in order to retrieve to correct values on ->
-  # -> an Active record query....we have to specify the class_name and the foreign_key
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
 
-  # A user can participate in many tasks.
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  # field :sign_in_count,      type: Integer, default: 0
+  # field :current_sign_in_at, type: Time
+  # field :last_sign_in_at,    type: Time
+  # field :current_sign_in_ip, type: String
+  # field :last_sign_in_ip,    type: String
+
+  ## Confirmable
+  # field :confirmation_token,   type: String
+  # field :confirmed_at,         type: Time
+  # field :confirmation_sent_at, type: Time
+  # field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+  ## Lockable
+  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :locked_at,       type: Time
+  has_many :owned_tasks, class_name: 'Task', foreign_key: 'owner_id' # Here in order to retrieve to correct values on ->
   has_many :participations, class_name: 'Participant' # Here we have to specify the model class name
-  has_many :tasks, through: :participations
-  # *------------------- Associations --------------------
+
+  def tasks
+    participations.includes(:task).map(&:task)
+  end
 end
